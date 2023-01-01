f03f340956b007a180e6d16cd2607203bfcb4574
Vlad2346543 <114310954+Vlad2346543@users.noreply.github.com>
2023-01-01 19:49:54 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,19 @@
+﻿#include <cstdio>
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double a, b, c;
+
+	cout << "Введіть перше число: ";
+	cin >> a;
+	cout << "Введіть друге число: ";
+	cin >> b;
+
+	c = a + b;
+
+	cout << "Результат: " << c << endl;
